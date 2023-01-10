b8f00f5020afb8b259ae63132919568dbfb06e08
Regislaine <nanafidelis@gmail.com>
2023-01-10 17:00:59 -0300

    refactor: add a funcao checkPassword no arquivo validdateLogin

--- src/validators/validateLogin.ts
+++ src/validators/validateLogin.ts
@@ -1,10 +1,21 @@
 import { Login } from '$/domain';
 import Joi from 'joi';
+import bcrypt from 'bcrypt';
 
 
 export const authValidators = {
   loginSchema: Joi.object<Login.Input>({
       email: Joi.string().email().required(),
       password: Joi.string().min(6).required()
-    })
+  }) as unknown,
+
+  async checkPassword(passwordBody: string, passwordDB: string) {
+    const verifyPass = await bcrypt.compare(passwordBody, passwordDB);
+    if (!verifyPass) {
+      const error = new Error('Senha inválida');
+      error.name = 'ValidationError';
+      throw error;
+    }
+    return verifyPass as boolean;
+  }
 };
