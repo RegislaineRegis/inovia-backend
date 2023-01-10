1bf69f10f8dcd1618bc0eaa476c6ed5247570b89
Regislaine <nanafidelis@gmail.com>
2023-01-10 16:43:40 -0300

    refactor: add função checkpassword, comparando as senhas de user e senha do db

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,6 @@
 import { User } from '$/domain';
 import { models } from '$/models/sequelize';
+import bcrypt from 'bcrypt';
 
 
 export const userService = {
@@ -8,10 +9,20 @@ export const userService = {
       { where: { email }, raw: true }
     );
     if (!userByEmail) {
-      const error = new Error('Email de usuário não encontrado');
+      const error = new Error('Email Inválido, usuário não encontrado');
       error.name = 'ValidationError';
       throw error;
     }
     return userByEmail as unknown as User;
+  },
+
+  async checkPassword(passwordBody: string, passwordDB: string) {
+    const verifyPass = await bcrypt.compare(passwordBody, passwordDB);
+    if (!verifyPass) {
+      const error = new Error('Senha inválida');
+      error.name = 'ValidationError';
+      throw error;
+    }
+    return verifyPass;
   }
 };
