bfca320a6a646d6aa55545bc1eeb2c045cf53664
Regislaine <nanafidelis@gmail.com>
2023-01-10 16:54:58 -0300

    refactor: remoção da função checkPassword do service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,5 @@
 import { User } from '$/domain';
 import { models } from '$/models/sequelize';
-import bcrypt from 'bcrypt';
 
 
 export const userService = {
@@ -14,15 +13,5 @@ export const userService = {
       throw error;
     }
     return userByEmail as unknown as User;
-  },
-
-  async checkPassword(passwordBody: string, passwordDB: string) {
-    const verifyPass = await bcrypt.compare(passwordBody, passwordDB);
-    if (!verifyPass) {
-      const error = new Error('Senha inválida');
-      error.name = 'ValidationError';
-      throw error;
-    }
-    return verifyPass;
   }
 };
