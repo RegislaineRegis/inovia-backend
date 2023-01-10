d2ab7dabc0714c9f4990e77358b5c389b2d49b1f
Regislaine <nanafidelis@gmail.com>
2023-01-10 17:31:11 -0300

    refactor:testando

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,10 +1,10 @@
-import { Login } from '$/domain';
+// import { Login } from '$/domain';
 import { authService } from '$/services/authService';
 import { userService } from '$/services/userService';
 import { authValidators } from '$/validators/validateLogin';
 
 export const authController = {
-  async postLogin(body: any): Promise<Login.Input> {
+  async postLogin(body: any) {
     const data = await authValidators.loginSchema.validateAsync(body);
     const user = await userService.getByEmail(data.email);
     const verifyPass = await authService.checkPassword(data.password, user.password);
