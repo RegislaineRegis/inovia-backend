fa757862c8adc35730b91e225db7a53585c71d02
Regislaine <nanafidelis@gmail.com>
2023-01-10 16:40:55 -0300

    refactor: chama a função checkPassword no postLogin

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,12 @@
 import { Login } from '$/domain';
-import { loginSchema } from '$/validators';
 import { userService } from '$/services/userService';
-
+import { authValidators } from '$/validators/validateLogin';
 
 export const authController = {
-  async postLogin(body: any): Promise<Login.LoginInput> {
-    const { email, password } = body;
-    await loginSchema.validateAsync({ email, password });
-    const user = await userService.getByEmail(email);
-    return user;
+  async postLogin(body: any): Promise<Login.Input> {
+    const data = await authValidators.loginSchema.validateAsync(body);
+    const user = await userService.getByEmail(data.email);
+    const verifyPass = await userService.checkPassword(data.password, user.password);
+    return verifyPass;
   }
 };
