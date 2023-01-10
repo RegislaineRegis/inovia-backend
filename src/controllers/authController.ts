93ccc712c1c6b86418bb99130a1289ecab9b4719
Regislaine <nanafidelis@gmail.com>
2023-01-10 17:14:36 -0300

    refactor: alteracao de importacao de arquivo authService

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Login } from '$/domain';
+import { authService } from '$/services/authService';
 import { userService } from '$/services/userService';
 import { authValidators } from '$/validators/validateLogin';
 
@@ -6,7 +7,7 @@ export const authController = {
   async postLogin(body: any): Promise<Login.Input> {
     const data = await authValidators.loginSchema.validateAsync(body);
     const user = await userService.getByEmail(data.email);
-    const verifyPass = await authValidators.checkPassword(data.password, user.password);
+    const verifyPass = await authService.checkPassword(data.password, user.password);
     return verifyPass;
   }
 };
