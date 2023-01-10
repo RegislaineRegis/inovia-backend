d7c1148026c9c376f66d5f91f16428659e7b955a
Regislaine <nanafidelis@gmail.com>
2023-01-10 12:25:24 -0300

    feat: criacao da rota postlogin

--- src/routes/api/auth.routes.ts
+++ src/routes/api/auth.routes.ts
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import { authController } from '$/controllers/authController';
+
 
 const authRoutes = Router();
+
+authRoutes.post('/', async (req, res) => {
+  const result = await authController.postLogin(req.body);
+  res.status(201).json(result);
+});
 
 export default authRoutes;
