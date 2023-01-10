d96975ef5082fe4319589641f70ca9a8e1db8db5
Regislaine <nanafidelis@gmail.com>
2023-01-10 17:04:40 -0300

    refactor: erro de digitacao

--- src/validators/validateLogin.ts
+++ src/validators/validateLogin.ts
@@ -7,7 +7,7 @@ export const authValidators = {
   loginSchema: Joi.object<Login.Input>({
       email: Joi.string().email().required(),
       password: Joi.string().min(6).required()
-  }) as unknown,
+  }),
 
   async checkPassword(passwordBody: string, passwordDB: string) {
     const verifyPass = await bcrypt.compare(passwordBody, passwordDB);
@@ -16,6 +16,6 @@ export const authValidators = {
       error.name = 'ValidationError';
       throw error;
     }
-    return verifyPass as boolean;
+    return verifyPass;
   }
 };
