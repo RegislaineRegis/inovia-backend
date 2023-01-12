4080842a8098c364dd2969e1b100997eab620e2b
Regislaine <nanafidelis@gmail.com>
2023-01-12 16:25:31 -0300

    refactor: refatorando e desustruturando user para evitar a quabra de segurança

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -9,26 +9,30 @@ export const authService = {
   async checkPassword(passwordBody: string, passwordDB: string): Promise<boolean> {
     const verifyPass = await bcrypt.compare(passwordBody, passwordDB);
     if (!verifyPass) {
-     UnauthorizedError('password is wrong');
+      UnauthorizedError('password is wrong');
     }
     return verifyPass;
   },
 
-  async createToke(user: User, timeAccessToken: string, timeRefreshToken: string): Promise<Login.Token> {
-    const accessToken = jwt.sign({ data: user }, jwtSecret, {
+  async createToke(user: User, timeAccessToken: string, timeRefreshToken: string): Promise<Login.Output> {
+    const { password: _, ...userInfo } = user;
+
+    const accessToken = jwt.sign({ data: userInfo }, jwtSecret, {
       expiresIn: timeAccessToken,
       algorithm: 'HS256'
     });
-    const refreshToken = jwt.sign({ data: user }, jwtSecret, {
+    const refreshToken = jwt.sign({ data: userInfo }, jwtSecret, {
       expiresIn: timeRefreshToken,
       algorithm: 'HS256'
     });
     const userToken = {
+      id: user.id,
+      email: user.email,
       accessToken,
       refreshToken,
       tokenType: 'Bearer',
       expiresIn: 2400
     };
-    return userToken as any as Login.Token;
+    return userToken as any as Login.Output;
   }
 };
