bc500049fa4b5243fae16d8897160f584b0af19b
Regislaine <nanafidelis@gmail.com>
2023-01-13 23:25:53 -0300

    refactor: correcao de tipagem

--- src/controllers/customerController.ts
+++ src/controllers/customerController.ts
@@ -2,7 +2,7 @@ import { Customer } from '$/domain';
 import { customerService } from '$/services/customerService';
 
 export const customerController = {
-  async getAll(): Promise<[Customer]> {
+  async getFullCustomer(): Promise<[Customer]> {
     const data = await customerService.getFullCustomer();
     return data as unknown as [Customer];
   }
