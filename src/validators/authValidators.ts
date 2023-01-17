71fefe94bfa51ffa5e67a9f16ea0b069c13dc48c
Regislaine <nanafidelis@gmail.com>
2023-01-17 12:28:37 -0300

    refactor: construção da validação do body do customer para add um novo customer

--- src/validators/authValidators.ts
+++ src/validators/authValidators.ts
@@ -1,4 +1,4 @@
-import { Login } from '$/domain';
+import { Customer, Login } from '$/domain';
 import Joi from 'joi';
 
 
@@ -8,5 +8,16 @@ export const authValidators = {
       password: Joi.string().min(6).required()
   }),
 
-  refresh: Joi.string().required().pattern(/Bearer\s.+/)
+  refresh: Joi.string().required().pattern(/Bearer\s.+/),
+
+  customer: Joi.object<Customer.Output>({
+    id: Joi.number().required(),
+    user_id: Joi.allow(null),
+    name: Joi.string().required(),
+    address: Joi.string().required(),
+    phone: Joi.string().required(),
+    email: Joi.string().email().required(),
+    birthDate: Joi.date().required(),
+    profilePhoto: Joi.string().required()
+  })
 };
