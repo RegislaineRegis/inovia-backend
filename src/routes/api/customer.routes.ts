33e69f2e7bb4058ee5a9a4c78690f5548e813a90
Regislaine <nanafidelis@gmail.com>
2023-01-17 12:24:38 -0300

    refactor: add a autenticacao do token no customerRoutes get

--- src/routes/api/customer.routes.ts
+++ src/routes/api/customer.routes.ts
@@ -1,10 +1,22 @@
 import { customerController } from '$/controllers/customerController';
+import { NotFoundError } from '$/middlewares/errors';
+import { authValidators } from '$/validators/authValidators';
 import { Router } from 'express';
 
 const customerRoutes = Router();
 
+customerRoutes.post('/', async (req, res) => {
+  const result = await customerController.addCustomer(req.body);
+  res.status(201).json(result);
+});
+
 customerRoutes.get('/', async (req, res) => {
+  const { authorization } = req.headers;
+  const validateAuth = await authValidators.refresh.validateAsync(authorization);
+  const token = validateAuth.split(' ');
+  if (!token) return NotFoundError('Token invalido');
   const result = await customerController.getFullCustomer();
   res.status(200).json(result);
 });
+
 export default customerRoutes;
